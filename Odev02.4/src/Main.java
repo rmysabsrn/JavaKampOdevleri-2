c8d126a6079fb80610e8f67a33e8e28529a968d1
Rumeysa Başaran <rumeysa.basaran@gmail.com>
2022-09-24 12:20:54 +0300

    value and referance types

--- Odev02.4/src/Main.java
+++ Odev02.4/src/Main.java
@@ -8,6 +8,21 @@ public class Main {
         customerManager.Remove();
         customerManager.Update();
 
+
+        //value
+        int sayi1 = 10;
+        int sayi2 = 20;
+        sayi2 = sayi1;
+        sayi1 = 30;
+        System.out.println(sayi2);
+
+        //diziler referans tipleri tutarlar
+        int[] sayilar1 = new int[] {1,2,3};
+        int[] sayilar2 = new int[] {4,5,6};
+        sayilar2 = sayilar1;
+        sayilar1[0] = 10;
+        System.out.println(sayilar2[0]);
+
     }
 }
 
