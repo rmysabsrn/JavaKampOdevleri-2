91f793644216456a71bb4202f644af36c11fb76b
Rumeysa Başaran <rumeysa.basaran@gmail.com>
2022-09-24 12:07:15 +0300

    classes2

--- Odev02.4/src/Main.java
+++ Odev02.4/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        //classlar referans tipidir.
+        CustomerManager customerManager = new CustomerManager();
+        CustomerManager customerManager2 = new CustomerManager();
+        customerManager = customerManager2;
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
 
     }
 }
