d11989cf00548fc8fe8b7d3f5f2457babb3ff1f8
Rumeysa Başaran <rumeysa.basaran@gmail.com>
2022-09-25 22:22:54 +0300

    getter and setter

--- Odev02.5/src/Product.java
+++ Odev02.5/src/Product.java
@@ -1,4 +1,5 @@
 public class Product {
+    //this içerisinde bulunduğun class demek
     //attribute | field
     private int id;
     private String name;
@@ -8,6 +9,57 @@ public class Product {
     private String renk;
     private String kod;
 
+    public int Id() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockAmount() {
+        return stockAmount;
+    }
+
+    public void setStockAmount(int stockAmount) {
+        this.stockAmount = stockAmount;
+    }
+
+    public String getRenk() {
+        return renk;
+    }
+
+    public void setRenk(String renk) {
+        this.renk = renk;
+    }
+
+    public String getKod() {
+        return this.name.substring(0,1)+id;
+    }
 
 }
--- Odev02.5/src/ProductManager.java
+++ Odev02.5/src/ProductManager.java
@@ -2,7 +2,7 @@ public class ProductManager {
 
     public void Add(Product product){
         //JDBS
-        System.out.println("Ürün eklendi: " + product.name);
+        System.out.println("Ürün eklendi: " + product.getName());
     }
 
 
