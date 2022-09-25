1ca681e34ae1c09811b79f182d2dfd2b34004810
Rumeysa Başaran <rumeysa.basaran@gmail.com>
2022-09-25 21:59:22 +0300

    Encapsulation

--- Odev02.5/src/Product.java
+++ Odev02.5/src/Product.java
@@ -1,10 +1,13 @@
 public class Product {
     //attribute | field
-    int id;
-    String name;
-    String description;
-    double price;
-    int stockAmount;
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int stockAmount;
+    private String renk;
+    private String kod;
+
 
 
 }
